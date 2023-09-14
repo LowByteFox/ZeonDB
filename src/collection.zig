3b8ce2b6a09d6f3db38095bdba592a53f802c897
LowByteFox <firethefox77@gmail.com>
2023-09-14 19:45:47 +0200

    unzeonlang

--- src/collection.zig
+++ src/collection.zig
@@ -1,25 +1,15 @@
 const std = @import("std");
 const trie = @import("./trie.zig");
 const types = @import("./types.zig");
-const exec = @import("./executor.zig");
 
 // A Collection is Trie with improved API and data encapsulation
 pub const Collection = struct {
     db: *trie.Trie(types.Value),
-    executor: ?exec.Executor,
 
     pub fn init(allocator: std.mem.Allocator) !Collection {
         return Collection{
             .db = try trie.Trie(types.Value).init(allocator),
-            .executor = null
         };
-    }
-
-    pub fn prepare_executor(self: *Collection, code: []const u8) void {
-        var e = exec.Executor.init(self, code);
-        if (e != null) {
-            self.executor = e;
-        }
     }
 
     pub fn deinit(self: *Collection, allocator: std.mem.Allocator) void {
--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,5 @@
 const std = @import("std");
 const collection = @import("collection.zig");
-const lex = @import("./parser/lexer.zig");
-const tk = @import("./parser/tokens.zig");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -11,8 +9,4 @@ pub fn main() !void {
 
     var db = try collection.Collection.init(allocator);
     defer db.deinit(allocator);
-
-    db.prepare_executor("set kluc hodnota");
-
-    try db.executor.?.exec(allocator);
 }

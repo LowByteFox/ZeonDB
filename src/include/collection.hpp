208663e00ec2387f496ed9b48bef1a8efe0cb97e
LowByteFox <firethefox77@gmail.com>
2024-01-11 09:22:16 +0100

    added webview and ZeonUI

--- src/include/collection.hpp
+++ src/include/collection.hpp
@@ -18,17 +18,22 @@ namespace ZeonDB {
 		struct Value;
 	}
 
+	using Map = std::unordered_map<std::string, std::shared_ptr<Types::Value>, wy::hash<std::string>>;
+
 	class Collection {
 		private:
-			std::unordered_map<std::string, std::shared_ptr<Types::Value>, wy::hash<std::string>> db;
+			std::map<std::string, Map> db;
 			std::map<std::string, Accounts::Permission> perms;
+			std::string def_ver = "default";
 
 		public:
 			void assign_perm(std::string, std::string, Accounts::Permission);
 			bool has_perms(std::string, std::string);
 			Accounts::Permission get_perms(std::string, std::string);
 			void add(std::string, std::shared_ptr<Types::Value>);
+			void add_at_version(std::string, std::shared_ptr<Types::Value>);
 			std::shared_ptr<Types::Value> get(std::string);
+			std::shared_ptr<Types::Value> get_at_version(std::string);
 			void iter(std::function<void(std::string, std::shared_ptr<Types::Value>)>);
 
 			std::string stringify(Types::FormatType, std::string, ZeonDB::Types::RecursionProtector*);

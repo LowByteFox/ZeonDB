cb64e87ceeefc643bff32d8744f65fd1f330ad2d
LowByteFox <firethefox77@gmail.com>
2024-02-05 22:15:03 +0100

    remove redundant project

--- ZeonUI/prepare.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-curl https://raw.githubusercontent.com/jagenjo/litegraph.js/master/build/litegraph.js > litegraph.js
-curl https://raw.githubusercontent.com/jagenjo/litegraph.js/master/css/litegraph.css > litegraph.css
